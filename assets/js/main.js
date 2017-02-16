97c8fa9dba4af260725c666229c119905e9b492d
melmdesign <liamm.designs@gmail.com>
2017-02-16 15:50:01 +0000

    Get Contentful Entries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
 // main.js
 var contentful = require('contentful')
+var client = contentful.createClient({
+  space: 'e3tkex9kqdfm',
+  accessToken: '7ec89f916560ccaaea0319417bbadfeffeafc010d3532f9c4dfd791ed82e9c97'
+
+})
+
+client.getEntries()
+.then(function (entries) {
+  // log the title for all the entries that have it
+  entries.items.forEach(function (entry) {
+    if(entry.fields.productName) {
+      console.log(entry.fields.productName)
+    }
+  })
+})
